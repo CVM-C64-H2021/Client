4ef0e170172e4acaed50d27b4a5ba1aa1de729c3
Jean-Philippe Lacroix <jeanphilippelacroix25@gmail.com>
2021-04-30 11:14:04 -0400

    update to datatable and correction to sensor data filter

--- pages/sensors.vue
+++ pages/sensors.vue
@@ -9,35 +9,16 @@
     </div>
 
     <div class="container">
-      <form id="search">
-        filtre: <input name="filter" v-model="searchQuery">
-      </form>
+      <DataTable 
+        :items="sensors" 
+        @click="logIt(sensor.messageAlerte + sensor.id)" 
+      />
     </div>
 
-    <div class="container">
-      <table id="laTable">
-        <thead>
-          <tr>
-            <th>Sensor</th>
-            <th>Date</th>
-            <th>Type</th>
-            <th>Valeur</th>
-            <th>Message</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="sensor in filteredData" :key="sensor.idApp" @click="logIt(sensor.messageAlerte + sensor.idApp)">
-            <td>{{sensor.idApp}}</td>
-            <td>{{sensor.date}}</td>
-            <td>{{sensor.type}}</td>
-            <td>{{sensor.valeur}}</td>
-            <td>{{sensor.messageAlerte}}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
   </div>
 </template>
+
+    
 
 <style scoped>
 .container {
@@ -49,61 +30,52 @@
 </style>
 
 <script>
-    console.log("patate");
-    let patate = 3
-  export default {
-    data () {
-      return {
-        searchQuery : "",
-        sensors :[
-    {
-        "idApp": 45454,
-        "date": "2020-01-01",
-        "type": "typetest",
-        "valeur": "valeurtest",
-        "alerte": false,
-        "messageAlerte": "message test"
-    },
-    {
-        "idApp": 66666,
-        "date": "2020-01-01",
-        "type": "Ceci est un type",
-        "valeur": "Ceci est une valeur",
-        "alerte": false,
-        "messageAlerte": "Alerte de message"
-    },
-    {
-        "idApp": 127891,
-        "date": "2020-01-06",
-        "type": "le type",
-        "valeur": "la valeur du senseur",
-        "alerte": false,
-        "messageAlerte": "ALARME"
-    }
-]
-      }
-    },
-    methods:{
-      logIt: function(name){
-        console.log(name);
-      }
-    },
-    computed: {
-    filteredData: function () {
-      // Valeur des caractères recherchés
-      const filterKey = this.filterKey && this.filterKey.toLowerCase()
-      let data = this.data
-      if (filterKey) {
-        data = data.filter(function (row) {
-          return Object.keys(row).some(function (key) {
-            return String(row[key]).toLowerCase().indexOf(filterKey) > -1
-          })
-        })
-      }
-      return data
+import DataTable from "@/components/DataTable"; 
+
+export default {
+  components: {
+    DataTable,
+  },
+  data () {
+    return {
+      sensors :[
+        {
+          "idApp": 45454,
+          "date": "2020-01-01",
+          "type": "typetest",
+          "valeur": "valeurtest",
+          "alerte": false,
+          "messageAlerte": "message test"
+        },
+        {
+          "idApp": 66666,
+          "date": "2020-01-01",
+          "type": "Ceci est un type",
+          "valeur": "Ceci est une valeur",
+          "alerte": false,
+          "messageAlerte": "Alerte de message"
+        },
+        {
+          "idApp": 127891,
+          "date": "2020-01-06",
+          "type": "le type",
+          "valeur": "la valeur du senseur",
+          "alerte": false,
+          "messageAlerte": "ALARME"
+        }   
+      ] 
     }
   },
+  methods:{
+    logIt: function(name){
+      console.log(name);
+    }
+  },
+  beforeMount() {
+      this.username = localStorage.getItem("username") || "USERNAMENOTFOUND404"
+      this.token = localStorage.getItem("token") || this.$router.push('login');   
   }
+}
 
   /*export default {
     data() {
